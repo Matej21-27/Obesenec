084494e19b8b8b9190271dd6d43dadd8c732e3e1
Matej21-27 <98655436+Matej21-27@users.noreply.github.com>
2022-05-15 11:07:47 +0200

    Create obesenec_funkcie.h

---
+++ obesenec_funkcie.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void greet();
+void display_chyby(int chyby);
+void display_stav(vector<char> nespravne, string odpoved);
+void end_hra(string odpoved, string Slovo);
